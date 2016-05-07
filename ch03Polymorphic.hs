1e330353756e793dba159ce2c892abe521e97b38
Ed Reade <ereadebanham@gmail.com>
2016-05-07 16:11:26 +0100

    major scales

--- ch03Polymorphic.hs
+++ ch03Polymorphic.hs
@@ -38,3 +38,46 @@ maxAbsPitch aps = foldl max 0 aps
 minAbsPitch :: [AbsPitch] -> AbsPitch
 minAbsPitch [] = error "Empty list"
 minAbsPitch aps = foldl min 0 aps
+
+chromScale :: Pitch -> Pitch -> [Pitch]
+chromScale p1 p2 | absPitch p1 == absPitch p2  = [p1]
+                 | absPitch p1 < absPitch p2   = p1 : chromScale (trans 1 p1) p2
+                 | otherwise                   = reverse (chromScale p2 p1)
+
+chrom :: Pitch -> Pitch -> Music Pitch
+chrom p1 p2 = foldr ((:+:) . note qn) (rest 0) (chromScale p1 p2)
+
+mkScale :: Pitch -> [Int] -> Music Pitch
+mkScale p [] = note en p
+mkScale p (int:ints) = note en p :+: mkScale (trans int p) ints
+
+data MajorScale = Ionian | Dorian | Phrygian | Lydian | Mixolydian |
+                  Aeolian | Locrian
+
+intervallicScale :: MajorScale -> [Int]
+intervallicScale ms = case ms of
+  Ionian     -> [2, 2, 1, 2, 2, 2, 1]
+  Dorian     -> [2, 1, 2, 2, 2, 1, 2]
+  Phrygian   -> [1, 3, 1, 2, 1, 2, 2]
+  Lydian     -> [2, 2, 2, 1, 2, 2, 1]
+  Mixolydian -> [2, 2, 1, 2, 2, 1, 2]
+  Aeolian    -> [2, 1, 2, 2, 1, 2, 2]
+  Locrian    -> [1, 2, 2, 1, 2, 2, 2]
+
+mkMajorScaleUp :: Pitch -> MajorScale -> Music Pitch
+mkMajorScaleUp p ms = mkScale p (init (intervallicScale ms))
+
+mkMajorScaleDown :: Pitch -> MajorScale -> Music Pitch
+mkMajorScaleDown p ms = let intervals = init . reverse . intervallicScale
+                        in  mkScale p (map negate (intervals ms))
+
+upDownMajorScale :: MajorScale -> Pitch -> Music Pitch
+upDownMajorScale ms p@(pc, o) = mkMajorScaleUp p ms :+:
+                                mkMajorScaleDown (pc, o + 1) ms
+
+allMajorScales :: Pitch -> Music Pitch
+allMajorScales p =
+  let scales = map upDownMajorScale [Ionian, Dorian, Phrygian, Lydian,
+                                     Mixolydian, Aeolian, Locrian]
+      variants = applyEach scales p
+  in foldl (:+:) (rest 0) variants :+: note wn p
