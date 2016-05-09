5d35b52bb696f5097f35611d0c06d39640bc337a
Ed Reade <ereadebanham@gmail.com>
2016-05-09 21:35:49 +0100

    freddie the frog

---
+++ ch03/Freddie.hs
@@ -0,0 +1,8 @@
+module Freddie where
+
+shift8Bit :: Int -> String -> String
+shift8Bit i = map (toEnum . mod 256 . (+) i . fromEnum)
+
+encrypt, decrypt :: String -> String
+encrypt = shift8Bit 1
+decrypt = shift8Bit (-1)
